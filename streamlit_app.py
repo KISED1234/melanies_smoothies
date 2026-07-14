0952f3192b2584b38bcdadda31cf7b086d50a793
KISED1234 <edkisby@gmail.com>
2026-07-14 12:02:10 +0100

    Remove st.stop() to continue app execution
    Removed the call to st.stop() to allow further execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 pd_df = my_dataframe.to_pandas()
 #below is only required for displaying table within app itself
 st.dataframe(pd_df)
-st.stop()
 
 # Multi select option as defined in https://docs.streamlit.io/develop/api-reference/widgets/st.multiselect. Also includes select all by default. My_dataframe picks up the above definition as defined by a Snowflake table column
 #ingredients is a variable and the data type is a list
